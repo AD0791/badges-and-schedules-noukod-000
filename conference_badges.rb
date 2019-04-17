ff2271b1e84d6fb339ffc3bb43f4680f370e4121
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-17 18:04:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator bc
 end
 
 def assign_rooms speaker 
-  speaker.each do |sp|
+  speaker.each_with_index do |sp|
     "Hello, #{sp}! You'll be assigned to room _____!"
   end
 end
