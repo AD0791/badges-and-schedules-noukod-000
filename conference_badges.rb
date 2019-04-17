f2407df36841bf393c66d16583409de6277b6163
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-17 18:09:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,11 +10,14 @@ def batch_badge_creator bc
   arrBadge
 end
 
-def assign_rooms(attendees)
+def assign_rooms attendees
   attendees.each_with_index.map do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
 end
 
-
+def printer att
+  batch_badge_creator(att).each do |badge|
+    puts badge
+end
 
